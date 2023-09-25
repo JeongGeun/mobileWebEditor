b8327caeeb85c155f70426fcd080c17ad4bfd062
JeongGeun <geun5310@naver.com>
2023-09-26 02:32:40 +0900

    feat: 신랑 신부 혼주 항목 추가

--- src/components/Template/A/Message/index.tsx
+++ src/components/Template/A/Message/index.tsx
@@ -13,6 +13,18 @@ export default function Message({ onSectionClick }: MessageProps) {
     <div className={styles.layout} onClick={onSectionClick}>
       <div className={styles.title}>{title || "제목은 여기에"}</div>
       <div className={styles.content}>{text || "문구는 여기에"}</div>
+      <div className={styles.info}>
+        <div className={styles.row}>
+          <div>조용오</div>
+          <span className={styles.bullet} />
+          <div>김현숙의 장남 정근</div>
+        </div>
+        <div className={styles.row}>
+          김체린
+          <span className={styles.bullet} />
+          김체린의 장녀 체린
+        </div>
+      </div>
     </div>
   );
 }
