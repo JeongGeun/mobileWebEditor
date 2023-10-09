c8023f2ec90e77cdcb37ed79c49d0e5e2f9901a1
JeongGeun <geun5310@naver.com>
2023-10-10 01:52:20 +0900

    feat: 슬라이더 이미지 리스트 추가

--- src/components/Template/A/Slider/index.tsx
+++ src/components/Template/A/Slider/index.tsx
@@ -1,9 +1,33 @@
 import styles from "./index.module.scss";
+import Image from "next/image";
+
+const mockArray = Array.from({ length: 12 }, (_, i) => String(i + 1));
 
 export default function Slider() {
   return (
     <div className={styles.layout}>
       <div className={styles.title}>갤러리</div>
+      <div className={styles.container}>
+        <div className={styles.outer}>
+          <div className={styles.wrapper}>
+            <ul>
+              {mockArray.map((num) => (
+                <li key={num}>
+                  <Image
+                    src={`https://www.itscard.co.kr/mobile/new_m/mcard/images/common/gallery_sample_${num.padStart(
+                      2,
+                      "0"
+                    )}.jpg?v=0.0.1`}
+                    alt="청첩장 사진1"
+                    width={230}
+                    height={343}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
