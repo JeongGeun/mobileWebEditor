aa95564df9f5de6eeb20c32600f7cced1cd88248
JeongGeun <geun5310@naver.com>
2023-10-18 02:04:34 +0900

    feat: active 되지 않은 이미지는 blur 처리

--- src/components/Template/A/Slider/index.tsx
+++ src/components/Template/A/Slider/index.tsx
@@ -2,13 +2,16 @@ import styles from "./index.module.scss";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Controller } from "swiper/modules";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type SwiperCore from "swiper";
+import cs from "classnames/bind";
 
+const cx = cs.bind(styles);
 const mockArray = Array.from({ length: 12 }, (_, i) => String(i + 1));
 
 export default function Slider() {
   const [controlledSwiper, setControlledSwiper] = useState<SwiperCore>();
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <div className={styles.layout}>
@@ -45,22 +48,29 @@ export default function Slider() {
               slidesPerView={5}
               spaceBetween={5}
               controller={{ control: controlledSwiper }}
-              onClick={(swiper) =>
-                controlledSwiper?.slideTo(swiper.clickedIndex)
-              }
+              onClick={(swiper) => {
+                controlledSwiper?.slideTo(swiper.clickedIndex);
+                setActiveIndex(swiper.clickedIndex);
+              }}
               noSwiping
             >
-              {mockArray.map((num) => (
+              {mockArray.map((num, index) => (
                 <SwiperSlide key={`mini_${num}`} className={styles.slide}>
-                  <Image
-                    src={`https://www.itscard.co.kr/mobile/new_m/mcard/images/common/gallery_sample_${num.padStart(
-                      2,
-                      "0"
-                    )}.jpg?v=0.0.1`}
-                    alt="청첩장 사진1"
-                    width={64}
-                    height={64}
-                  />
+                  <div
+                    className={cx({
+                      blur: index !== activeIndex,
+                    })}
+                  >
+                    <Image
+                      src={`https://www.itscard.co.kr/mobile/new_m/mcard/images/common/gallery_sample_${num.padStart(
+                        2,
+                        "0"
+                      )}.jpg?v=0.0.1`}
+                      alt="청첩장 사진1"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
                 </SwiperSlide>
               ))}
             </Swiper>
