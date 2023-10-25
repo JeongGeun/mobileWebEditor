24b41dd0fa073938c3ef8c0335c59400d73de442
JeongGeun <geun5310@naver.com>
2023-10-26 01:27:41 +0900

    feat: sliderInspector컴포넌트 추가

---
+++ src/components/Inspector/Slider/index.tsx
@@ -0,0 +1,44 @@
+import styles from "./index.module.scss";
+import { UploadOutlined } from "@ant-design/icons";
+import type { UploadProps } from "antd";
+import { Button, Upload } from "antd";
+
+const props: UploadProps = {
+  onChange({ file, fileList }) {
+    if (file.status !== "uploading") {
+      console.log(file, fileList);
+    }
+  },
+  defaultFileList: [
+    {
+      uid: "1",
+      name: "xxx.png",
+      status: "uploading",
+      url: "http://www.baidu.com/xxx.png",
+      percent: 33,
+    },
+    {
+      uid: "2",
+      name: "yyy.png",
+      status: "done",
+      url: "http://www.baidu.com/yyy.png",
+    },
+    {
+      uid: "3",
+      name: "zzz.png",
+      status: "error",
+      response: "Server Error 500", // custom error message to show
+      url: "http://www.baidu.com/zzz.png",
+    },
+  ],
+};
+
+export default function SliderInspector() {
+  return (
+    <div className={styles.layout}>
+      <Upload {...props}>
+        <Button icon={<UploadOutlined />}>Upload</Button>
+      </Upload>
+    </div>
+  );
+}
