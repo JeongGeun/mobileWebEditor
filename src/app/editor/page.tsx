2d0a5cd143bb8bab1b631db4f0e45da206d29003
JeongGeun <geun5310@naver.com>
2024-01-05 01:18:06 +0900

    feat: edit 페이지에서 저장이력 불러오기 기능 추가

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -3,8 +3,23 @@
 import { Layout } from "antd";
 import Renderer from "@/components/Renderer";
 import Sider from "@/components/Sider";
+import { useSearchParams } from "next/navigation";
+import { useGetInviInfoQuery } from "@/query/useGetInviInfoQuery";
+import { useEffect } from "react";
+import { useForm, useFormContext } from "react-hook-form";
 
 export default function Editor() {
+  const { watch, reset } = useFormContext();
+  const params = useSearchParams();
+  const id = params.get("id") as string;
+  const { data } = useGetInviInfoQuery(id, { enabled: !!id });
+
+  useEffect(() => {
+    if (data) {
+      reset(data);
+    }
+  }, [reset, data]);
+
   return (
     <Layout style={{ height: "100%" }} hasSider>
       <Layout.Content>
