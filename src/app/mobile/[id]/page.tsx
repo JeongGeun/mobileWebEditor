4a29b36dd57ec8a7b1048f57d507012a79d1204b
JeongGeun <geun5310@naver.com>
2024-01-21 22:46:50 +0900

    feat: 모바일용 path 분리

--- src/app/mobile/[id]/page.tsx
+++ src/app/mobile/[id]/page.tsx
@@ -1,5 +1,24 @@
+import { getInvitatationInfo } from "@/apis/list";
+import getQueryClient from "@/app/getQueryClient";
+import MobileRender from "@/components/MobileRenderer";
+import { GET_INVI_INFO_QUERY_KEY } from "@/query/useGetInviInfoQuery";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 
+export default async function MobilePage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const queryClient = getQueryClient();
+  const id = params.id;
+  await queryClient.prefetchQuery({
+    queryKey: [GET_INVI_INFO_QUERY_KEY, id],
+    queryFn: () => getInvitatationInfo(id),
+  });
 
-export default function MobilePage() {
-  return null;
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <MobileRender id={id} />
+    </HydrationBoundary>
+  );
 }
