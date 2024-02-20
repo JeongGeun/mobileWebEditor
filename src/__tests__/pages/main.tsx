fa5be36b3d75cc11b6472c5695f553068efe0804
JeongGeun <geun5310@naver.com>
2024-02-21 01:58:59 +0900

    test: next/navigaiton mock 함수 및 테스트 추가

--- src/__tests__/pages/main.tsx
+++ src/__tests__/pages/main.tsx
@@ -1,18 +1,39 @@
 import MainPage from '@/app/main/page'
 import { TestWrapper } from '@/util/test/wrapper';
-
 import { render, fireEvent, screen } from '@testing-library/react'
+import { useRouter } from "next/navigation";
 
 jest.mock("next/navigation", () => ({
-    useRouter() {
-        return {
-            prefetch: () => null
-        };
-    }
+    useRouter: jest.fn()
 }));
 
-test("Main 페이지의 타이틀이 모청 리스트 인지 확인한다.", () => {
-    render(<MainPage />, { wrapper: TestWrapper() })
-    const header = screen.getByRole('heading');
-    expect(header).toHaveTextContent("모청 리스트")
+
+const mockRouter = {
+    push: jest.fn() // the component uses `router.push` only
+};
+(useRouter as jest.Mock).mockReturnValue(mockRouter);
+
+
+describe('Main 페이지', () => {
+
+    beforeEach(() => {
+        render(<MainPage />, { wrapper: TestWrapper() });
+
+    })
+
+    it("타이틀이 모청 리스트 인지 확인한다.", () => {
+        const header = screen.getByRole('heading');
+        expect(header).toHaveTextContent("모청 리스트");
+    })
+
+    it("등록하기 버튼 클릭 시 next/navigation의 push 함수를 실행하고 editor 페이지로 이동한다.", () => {
+        const registrationButton = screen.getByRole("button");
+        fireEvent.click(registrationButton);
+        expect(mockRouter.push).toHaveBeenCalledTimes(1);
+        expect(mockRouter.push).toHaveBeenCalledWith('/editor');
+    });
+
+
+
+
 })
